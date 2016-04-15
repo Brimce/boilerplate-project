46e216f0742767875f6676d2554adb00550ff2e1
Brimce <bricechapellier@hotmail.com>
2016-04-15 19:54:10 +0200

    merge de featuremodule_redux

--- app/containers/main_container.jsx
+++ app/containers/main_container.jsx
@@ -1,65 +1,57 @@
-import React, { Component, PropTypes } from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import Counter from '../components/counter_react'
-import LoaderReact from '../components/loader_react'
-import ErrorContainer from '../containers/error_container'
+import React, {Component, PropTypes} from 'react'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+import {Button, FABButton, Icon} from 'react-mdl';
+
+import CounterView from '../module_redux/counter/CounterView'
+import LoaderView from '../module_redux/loader/LoaderView'
+import ErrorView from '../module_redux/error/ErrorView'
+//import ErrorContainer from '../containers/error_container'
 import * as CounterActions from '../actions/action_creators'
 import DevTools from './DevTools';
 
-if ( process.env.BROWSER ) {
+if (process.env.BROWSER) {
     require('./../asset/main.css');
     require('./../asset/material.min.css');
     require('./../asset/material.min.js');
 }
 
-class MainContainer extends React.Component{
+class MainContainer extends React.Component {
 
-    render(){
+    render() {
         const isProduction = process.env.NODE_ENV === 'production';
-
+        const {undo, redo} = this.props;
         return (
             <div>
-                <LoaderReact isLoading={this.props.isLoading} />
-                <Counter counter={this.props.counter}
-                         increment={this.props.increment}
-                         decrement={this.props.decrement}
-                         incrementIfOdd={this.props.incrementIfOdd}
-                         incrementAsync={this.props.incrementAsync}
-                         undo={this.props.undo}
-                         redo={this.props.redo} />
-                    {/*isProduction ? null : <DevTools />*/}
-                 <ErrorContainer/>
-                 <br/>
+                <LoaderView/>
+                <ErrorView/>
+                <Button onClick={undo}>Undo</Button>
+                {' '}
+                <Button onClick={redo}>Redo</Button>
+                <CounterView/>
+                <br/>
                 <span>{`1 : Production ? ${isProduction}`}</span>
-                 <br/>
-                {isProduction ? null : <DevTools />}
-</div>
+                <br/> {isProduction
+                    ? null
+                    : <DevTools/>}
+            </div>
         );
     }
 }
 
 //inject des propriet�s du store dans les propri�t�s du container
 function mapStateToProps(state) {
-  return {
-      counter: state.CounterReducer.present.count,
-      isLoading: state.MainReducer.isLoading,
-      error: state.ErrorReducer
-  }
+    return {}
 }
 
 //inject des actionCreators dans les proprietes du container
 function mapDispatchToProps(dispatch) {
     return bindActionCreators({
-        increment: CounterActions.increment,
-        decrement: CounterActions.decrement,
-        incrementAsync: CounterActions.incrementAsync,
-        incrementIfOdd: CounterActions.incrementIfOdd,
-        showLoader: CounterActions.showLoading,
         undo: CounterActions.undo,
         redo: CounterActions.redo
     }, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(MainContainer)
+//export default connect(mapStateToProps, mapDispatchToProps)(MainContainer)
 //export default connect(mapStateToProps)(MainContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(MainContainer);
